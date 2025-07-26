d86c3a6a44f18d5a82163a613343a6b6249ea174
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2025-07-26 18:51:38 -0400

    defined models

--- app/models/page.go
+++ app/models/page.go
@@ -1,6 +1,7 @@
 package models
 
 // TODO: Import the required "time" package for timestamp fields
+import "time"
 
 // TODO: Create a Page struct that will represent pages in our CMS
 // This struct should include fields for:
@@ -11,25 +12,30 @@ package models
 // - UpdatedAt (timestamp for last update)
 
 type Page struct {
-    // TODO: Add ID field as uint with:
-    // - gorm tag for primary key
-    // - json tag for serialization
-    
-    // TODO: Add Title field as string with:
-    // - gorm tags for size limit (255) and not null constraint
-    // - json tag for serialization
-    // - binding tag to make it required
-    
-    // TODO: Add Content field as string with:
-    // - gorm tag specifying text type and not null constraint
-    // - json tag for serialization
-    // - binding tag to make it required
-    
-    // TODO: Add CreatedAt field using time.Time with:
-    // - gorm tag for automatic timestamp on creation
-    // - json tag for serialization
-    
-    // TODO: Add UpdatedAt field using time.Time with:
-    // - gorm tag for automatic timestamp on updates
-    // - json tag for serialization
+	// TODO: Add ID field as uint with:
+	// - gorm tag for primary key
+	// - json tag for serialization
+	ID uint `gorm:"primaryKey" json:"id"`
+
+	// TODO: Add Title field as string with:
+	// - gorm tags for size limit (255) and not null constraint
+	// - json tag for serialization
+	// - binding tag to make it required
+	Title string `gorm:"size:255;not null" json:"title" binding:"required"`
+
+	// TODO: Add Content field as string with:
+	// - gorm tag specifying text type and not null constraint
+	// - json tag for serialization
+	// - binding tag to make it required
+	Content string `gorm:"type:text;not null" json:"content" binding:"required"`
+
+	// TODO: Add CreatedAt field using time.Time with:
+	// - gorm tag for automatic timestamp on creation
+	// - json tag for serialization
+	Createdat time.Time `gorm:"autoCreateTime" json:"created_at"`
+
+	// TODO: Add UpdatedAt field using time.Time with:
+	// - gorm tag for automatic timestamp on updates
+	// - json tag for serialization
+	UpodatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
--- app/models/post.go
+++ app/models/post.go
@@ -1,6 +1,7 @@
 package models
 
 // TODO: Import the required "time" package for timestamp fields
+import "time"
 
 // TODO: Create a Post struct that will represent blog posts in our CMS
 // This struct should include fields for:
@@ -13,33 +14,39 @@ package models
 // - Media (slice of Media, representing a many-to-many relationship)
 
 type Post struct {
-    // TODO: Add ID field as uint with:
-    // - gorm tag for primary key
-    // - json tag for serialization
-    
-    // TODO: Add Title field as string with:
-    // - gorm tags for size limit (255) and not null constraint
-    // - json tag for serialization
-    // - binding tag to make it required
-    
-    // TODO: Add Content field as string with:
-    // - gorm tag specifying text type and not null constraint
-    // - json tag for serialization
-    // - binding tag to make it required
-    
-    // TODO: Add Author field as string with:
-    // - gorm tag for size limit (100)
-    // - json tag for serialization
-    
-    // TODO: Add CreatedAt field using time.Time with:
-    // - gorm tag for automatic timestamp on creation
-    // - json tag for serialization
-    
-    // TODO: Add UpdatedAt field using time.Time with:
-    // - gorm tag for automatic timestamp on updates
-    // - json tag for serialization
-    
-    // TODO: Add Media field as []Media with:
-    // - gorm tag for many-to-many relationship (specify junction table name: post_media)
-    // - json tag for serialization
+	// TODO: Add ID field as uint with:
+	// - gorm tag for primary key
+	// - json tag for serialization
+	ID uint `gorm:"primaryKey" json:"id"`
+	// TODO: Add Title field as string with:
+	// - gorm tags for size limit (255) and not null constraint
+	// - json tag for serialization
+	// - binding tag to make it required
+	Title string `gorm:"size:255" json:"title" binding:"required"`
+
+	// TODO: Add Content field as string with:
+	// - gorm tag specifying text type and not null constraint
+	// - json tag for serialization
+	// - binding tag to make it required
+	Content string `gorm:"type:text;not null" json:"content" binding:"required"`
+
+	// TODO: Add Author field as string with:
+	// - gorm tag for size limit (100)
+	// - json tag for serialization
+	Author string `gorm:"size:100" json:"author"`
+
+	// TODO: Add CreatedAt field using time.Time with:
+	// - gorm tag for automatic timestamp on creation
+	// - json tag for serialization
+	Createdat time.Time `gorm:"autoCreateTime" json:"created_at"`
+
+	// TODO: Add UpdatedAt field using time.Time with:
+	// - gorm tag for automatic timestamp on updates
+	// - json tag for serialization
+	UpodatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+
+	// TODO: Add Media field as []Media with:
+	// - gorm tag for many-to-many relationship (specify junction table name: post_media)
+	// - json tag for serialization
+	Media []Media `gorm:"many2many:post_media" json:"media"`
 }
